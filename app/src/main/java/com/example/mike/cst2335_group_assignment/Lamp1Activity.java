7bea8ee732f78c2f123192196da6446c89e06b36
Mike Gordanier <gord0213@algonquinlive.com>
2016-11-22 16:48:03 -0500

    Added Different Activitys

--- app/src/main/java/com/example/mike/cst2335_group_assignment/Lamp1Activity.java
+++ app/src/main/java/com/example/mike/cst2335_group_assignment/Lamp1Activity.java
@@ -1,14 +1,17 @@
 package com.example.mike.cst2335_group_assignment;
 
+import android.icu.text.DateFormat;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ToggleButton;
 
 public class Lamp1Activity extends AppCompatActivity {
-
+    ToggleButton toggleButton;
+    Boolean ButtonState;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +19,8 @@ public class Lamp1Activity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        toggleButton = (ToggleButton)findViewById(R.id.LightSwitch);
+        ButtonState = toggleButton.isChecked();
 
     }
 
