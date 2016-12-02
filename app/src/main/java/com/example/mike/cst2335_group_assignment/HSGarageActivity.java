cba631dd7083086b9777ace130385a38c1adbf12
Jeff McNally <mcna0242@algonquinlive.com>
2016-12-02 00:34:53 -0500

    Updated strings, HouseSettings.java, Added HSGarageActivity, HSHouseTempActivity, HSOutsideTempActivity, and related layouts.

--- app/src/main/java/com/example/mike/cst2335_group_assignment/HSGarageActivity.java
+++ app/src/main/java/com/example/mike/cst2335_group_assignment/HSGarageActivity.java
@@ -1,17 +1,71 @@
 package com.example.mike.cst2335_group_assignment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HSGarageActivity extends AppCompatActivity {
     protected static final String ACTIVITY_NAME = "HSGarageActivity";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hsgarage);
         Log.i(ACTIVITY_NAME, "In onCreate()");
+        Button btnOpenClose = (Button) findViewById(R.id.btnOpenClose);
+        final Switch swtGarageLight = (Switch) findViewById(R.id.swtGarageLight);
+        final TextView txtGarageStatus = (TextView) findViewById(R.id.txtGarageStatus);
+
+        //button for opening and closing the garage
+        btnOpenClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Context context = HSGarageActivity.this;
+                if (txtGarageStatus.getText() == getString(R.string.gs_closed)){
+                    txtGarageStatus.setText(getString(R.string.gs_open));
+                   //if door is being opened, turn on the light
+                    swtGarageLight.setChecked(true);
+                } else {
+                    txtGarageStatus.setText(getString(R.string.gs_closed));
+                }
+               //save current state to shared prefs.
+                SharedPreferences prefs = getSharedPreferences("cst2335_group_assignment", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putString("GarageDoor", txtGarageStatus.getText().toString());
+                editor.commit();
+            }
+        });
+        //turn on or off the garage light with switch. save state in shared prefs.
+        swtGarageLight.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences prefs = getSharedPreferences("cst2335_group_assignment", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = prefs.edit();
+                if (isChecked) {
+
+                    Toast toast = Toast.makeText(getApplicationContext(), "Garage Light is On", Toast.LENGTH_SHORT);
+                    toast.show();
+                    editor.putBoolean("GarageLight", true);
+                    editor.commit();
+
+                } else {
+
+                    Toast toast = Toast.makeText(getApplicationContext(), "Garage Light is Off", Toast.LENGTH_SHORT);
+                    toast.show();
+                    editor.putBoolean("GarageLight", false);
+                    editor.commit();
+                }
+            }
+        });
     }
 
     @Override
@@ -24,6 +78,16 @@ public class HSGarageActivity extends AppCompatActivity {
     protected void onStart(){
         super.onStart();
         Log.i(ACTIVITY_NAME, "In onStart()");
+        Switch swtGarageLight = (Switch) findViewById(R.id.swtGarageLight);
+        TextView txtGarageStatus = (TextView) findViewById(R.id.txtGarageStatus);
+
+//        TextView txtGarageStatus = (TextView) findViewById(R.id.txtGarageStatus);
+        SharedPreferences prefs = getSharedPreferences("cst2335_group_assignment", Context.MODE_PRIVATE);
+        txtGarageStatus.setText( prefs.getString("GarageDoor", getString(R.string.gs_closed)));
+
+//        Switch swtGarageLight = (Switch) findViewById(R.id.swtGarageLight);
+//        SharedPreferences prefsGarageLight = getSharedPreferences("GarageLight", Context.MODE_PRIVATE);
+        swtGarageLight.setChecked( prefs.getBoolean("GarageLight", false));
 
     }
 
--- app/src/main/java/com/example/mike/cst2335_group_assignment/HSHouseTempActivity.java
+++ app/src/main/java/com/example/mike/cst2335_group_assignment/HSHouseTempActivity.java
@@ -1,16 +1,112 @@
 package com.example.mike.cst2335_group_assignment;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import static java.lang.Integer.valueOf;
 
 public class HSHouseTempActivity extends AppCompatActivity {
     protected static final String ACTIVITY_NAME = "HSHouseTempActivity";
+    public int houseTemp;
+    ArrayList<String> tempAdapterList;
+    TempAdapter tempAdapter;
+//    TempDatabaseHelper tempHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hshouse_temp);
+        SharedPreferences prefs = getSharedPreferences("cst2335_group_assignment", Context.MODE_PRIVATE);
+        ListView schedTempView = (ListView) findViewById(R.id.schedTempView);
+        houseTemp = prefs.getInt("HouseTemp", 22);
+        TextView txtHouseTemp = (TextView) findViewById(R.id.txtHouseTemp);
+        SeekBar sbSetTemp = (SeekBar) findViewById(R.id.sbSetTemp);
+        final EditText setTemp = (EditText) findViewById(R.id.setTemp);
+        final EditText setTime = (EditText) findViewById(R.id.setTime);
+        Button btnSetTemp = (Button) findViewById(R.id.btnSetTemp);
+        tempAdapterList = new ArrayList<String>();
+
+        tempAdapter = new TempAdapter(this);
+        schedTempView.setAdapter(tempAdapter);
+
+        txtHouseTemp.setText(String.valueOf(houseTemp) + "ºC");
+        sbSetTemp.setProgress(valueOf(houseTemp));
+
+        //seek bar to adjust the house temperature
+        sbSetTemp.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                TextView txtHouseTemp = (TextView) findViewById(R.id.txtHouseTemp);
+                houseTemp = progress;
+                txtHouseTemp.setText(String.valueOf(houseTemp) + "ºC");
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // Once touch has been released, save temp into shared prefs.
+                SharedPreferences prefs = getSharedPreferences("cst2335_group_assignment", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putInt("HouseTemp", valueOf(houseTemp));
+                editor.commit();
+            }
+        });
+
+        btnSetTemp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View view){
+                tempAdapterList.add("Time: " + setTime.getText().toString() + ", Temp: " + setTemp.getText().toString());
+//                tempHelper.insertData(tempText.getText().toString());
+                tempAdapter.notifyDataSetChanged(); //this restarts the process of getCount()/ getView()
+//                tempText.setText("");
+            }
+        });
+    }
+
+    private class TempAdapter extends ArrayAdapter<String> {
+        public TempAdapter(Context ctx){
+            super(ctx, 0);
+        }
+
+        public int getCount(){
+            return tempAdapterList.size();
+        }
+
+        public String getItem(int position){
+            return tempAdapterList.get(position);
+        }
+
+       public View getView(int position, View convertView, ViewGroup parent){
+            LayoutInflater inflater = HSHouseTempActivity.this.getLayoutInflater();
+
+            View result = null ;
+//            if(position%2 == 0) {
+                result = inflater.inflate(R.layout.scheduled_temp_list, null);
+//            } else {
+//                result = inflater.inflate(R.layout.chat_row_outgoing, null);
+//            }
+            TextView temp = (TextView)result.findViewById(R.id.temp_text);
+            temp.setText(   getItem(position)  ); // get the string at position
+            return result;
+
+        }
+
     }
 
     @Override
@@ -23,6 +119,10 @@ public class HSHouseTempActivity extends AppCompatActivity {
     protected void onStart(){
         super.onStart();
         Log.i(ACTIVITY_NAME, "In onStart()");
+ //       SharedPreferences prefs = getSharedPreferences("cst2335_group_assignment", Context.MODE_PRIVATE);
+
+
+
 
     }
 
